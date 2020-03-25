617a9bee919b707cfa78bbfefd9c26eb2412b662
Hsieh Ching Hsiang <vita_hsieh@morrisonexpress.com>
2020-03-25 23:38:06 +0800

    Adjust Heder for Functional Component Date 0325

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,30 @@
 import React from 'react'
 
-class Header extends React.Component {
-  renderLink() {
-    const nickName = this.props.nickName
-    if (nickName) {
-      return (
-        <span className='nickname'>
-          <i className='far fa-user'></i>
-          {this.props.nickName}
-        </span>
-      )
-    } else {
-      return (
-        <React.Fragment>
-          <a href='/'>Login</a>
-          <a href='/'>Register</a>
-        </React.Fragment>
-      )
-    }
-  }
-
-  render() {
-    return (
-      <div className='header'>
-        <div className='grid'>
-          <div className='start'>
-            <a href='/'>Home</a>
-          </div>
-          <div className='end'>
-            {this.renderLink()}
-          </div>
+const Header = (props) => {
+  return (
+    <div className='header'>
+      <div className='grid'>
+        <div className='start'>
+          <a href='/'>Home</a>
+        </div>
+        <div className='end'>
+          {
+            props.nickName ? (
+              <span className='nickname'>
+                <i className='far fa-user'></i>
+                {props.nickName}
+              </span>
+            ) : (
+              <React.Fragment>
+                <a href='/'>Login</a>
+                <a href='/'>Register</a>
+              </React.Fragment>
+            )
+          }
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Header
