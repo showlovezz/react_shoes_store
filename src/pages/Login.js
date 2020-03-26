9cf86f72f5867875d4fc19c473f62920f94bfac1
Hsieh Ching Hsiang <vita_hsieh@morrisonexpress.com>
2020-03-26 12:47:41 +0800

    Add Login Component Event Date 0326

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,10 +2,28 @@ import React from 'react'
 
 // 注意 React.Component 大小寫，曾經卡在這
 class Login extends React.Component {
+  // 事件注意事項
+  // 1. 命名和綁定
+  // 2. event
+  // 3. this
+  // 4. 傳遞參數
+
+  handleSubmit = (event) => {
+    // 1. 阻止默認行為
+    event.preventDefault()
+
+    // 2. 獲取表單數據
+
+    // 3. 處理登入邏輯
+
+    // 4. 跳轉到首頁頁面
+    this.props.history.push('/')
+  }
+
   render() {
     return (
       <div className='login-wrapper'>
-        <form className='box login-box'>
+        <form className='box login-box' onSubmit={this.handleSubmit}>
           <div className='field'>
             <label className='label'>Email</label>
             <div className='control'>
