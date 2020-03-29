2c8ff8cf6e029de28bbfc801ae7ef58a845d2bab
Hsieh Ching Hsiang <vita_hsieh@morrisonexpress.com>
2020-03-29 23:45:25 +0800

    Use Refs for Email and Password Date 0329

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,17 +2,19 @@ import React from 'react'
 
 // 注意 React.Component 大小寫，曾經卡在這
 class Login extends React.Component {
-  // 事件注意事項
-  // 1. 命名和綁定
-  // 2. event
-  // 3. this
-  // 4. 傳遞參數
+  // Refs
+  emailRef = React.createRef()
+  passwordRef = React.createRef()
 
   handleSubmit = (event) => {
     // 1. 阻止默認行為
     event.preventDefault()
 
     // 2. 獲取表單數據
+    const formData = {
+      email: this.emailRef.current.value,
+      password: this.passwordRef.current.value,
+    }
 
     // 3. 處理登入邏輯
 
@@ -27,13 +29,13 @@ class Login extends React.Component {
           <div className='field'>
             <label className='label'>Email</label>
             <div className='control'>
-              <input className='input' type='text' placeholder='Email' />
+              <input className='input' type='text' placeholder='Email' ref={this.emailRef} />
             </div>
           </div>
           <div className='field'>
             <label className='label'>Password</label>
             <div className='control'>
-              <input className='input' type='password' placeholder='Password' />
+              <input className='input' type='password' placeholder='Password' ref={this.passwordRef} />
             </div>
           </div>
           <div className='control'>
